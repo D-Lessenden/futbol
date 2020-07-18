5338d408068054045e9625faf5b556671dc11aa7
Arique Aguilar <arique1104@gmail.com>
2020-07-18 15:49:04 -0500

    StatTracker

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,17 +1,26 @@
 require "CSV"
+require "./lib/games"
+
 class StatTracker
- attr_reader  :locations
+  attr_reader :games
 
   def self.from_csv(locations)
     StatTracker.new(locations)
-    @game = CSV.read(locations[:games])
-
-    require "pry"; binding.pry
-    self.new
   end
 
   def initialize(locations)
-    @locations = locations
+    @games ||= turn_games_csv_data_into_games_objects(locations[:games])
+    # @teams = locations[:teams]
+    # @game_teams = locations[:game_teams]
+  end
+
+  def turn_games_csv_data_into_games_objects(games_csv_data)
+    games_objects_collection = []
+    CSV.foreach(games_csv_data, headers: true, header_converters: :symbol) do |row|
+      games_objects_collection << Games.new(row)
+    end
+    games_objects_collection
+    # require "pry"; binding.pry
   end
 
 end
--- test/games_test.rb
+++ test/games_test.rb
@@ -3,17 +3,28 @@ require "minitest/pride"
 require "mocha/minitest"
 require "./lib/stat_tracker"
 require "./lib/games"
+require "CSV"
 
 class GamesTest < Minitest::Test
 
   def test_it_exists
-    stat_tracker = StatTracker.new(locations)
-    @games_csv = stat_tracker.from_csv
-    games = Games.new(csv_file)
+    games_location = './data/games.csv'
+
+    games = Games.new(games_location)
     assert_instance_of Games, games
   end
 
-  #def test_it_has_attributes
-  #end
+  def test_it_can_read_csv_doc
+    games_location = './data/games.csv'
+    games = Games.new(games_location)
+    games.from_csv
+
+  end
+
+  # def test_it_has_attributes
+  #   # assert_equal 2012030221, games.game_id
+  #   # assert_equal 20122013, games.season
+  #
+  # end
 
 end
