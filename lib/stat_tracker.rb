f7126a210b34a5405ad7167e37eb45a62d33b946
Nico Rithner <nicorithner@gmail.com>
2020-07-19 12:24:03 -0600

    Add game_teams.rb and start of percentage home wins test

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -9,6 +9,7 @@ class StatTracker
 
   def initialize(locations)
     @games ||= turn_games_csv_data_into_games_objects(locations[:games])
+    @game_teams ||= turn_games_csv_data_into_games_objects(locations[:game_teams])
     # @teams = locations[:teams]
     # @game_teams = locations[:game_teams]
   end
@@ -19,6 +20,15 @@ class StatTracker
       games_objects_collection << Games.new(row)
     end
     games_objects_collection
+    # require "pry"; binding.pry
+  end
+
+  def turn_games_csv_data_into_game_teams_objects(game_teams_csv_data)
+    game_teams_objects_collection = []
+    CSV.foreach(game_teams_csv_data, headers: true, header_converters: :symbol) do |row|
+      game_teams_objects_collection << GameTeams.new(row)
+    end
+    game_teams_objects_collection
     # require "pry"; binding.pry
   end
 
@@ -35,4 +45,10 @@ class StatTracker
     end
     output.total_game_score
   end
+
+  def percentage_home_wins
+    # binding.pry
+    @game_teams
+  end
+
 end
