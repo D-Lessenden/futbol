f21422ec6e33603bd4f3c55cb546a10750295202
Garret Wolf <garretwolf26@gmail.com>
2020-07-28 13:57:00 -0400

    Add highest and lowest scoring visitor methods

--- lib/league_statistics.rb
+++ lib/league_statistics.rb
@@ -43,4 +43,23 @@ class LeagueStatistics
     id = goals.min_by {|team, num_of_goals| num_of_goals}
     @teams.find {|team| team.team_id == id[0]}.teamname
   end
+
+  def highest_scoring_visitor
+    best_team = overall_average_scores_by_away_team.max_by do |team_id, average_goals|
+        average_goals
+    end
+    @teams.find do |team|
+      team.team_id == best_team[0]
+    end.teamname
+  end
+
+  def lowest_scoring_visitor
+    worst_team = overall_average_scores_by_away_team.min_by do |team_id, average_goals|
+      average_goals
+    end
+    @teams.find do |team|
+      team.team_id == worst_team[0]
+    end.teamname
+  end
+
 end
