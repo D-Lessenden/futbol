95195ece275da083730f70b16c4e7576a120de6e
Garret Wolf <garretwolf26@gmail.com>
2020-07-28 12:58:51 -0400

    Add setup for the league statistics tests

---
+++ test/league_statistics_test.rb
@@ -0,0 +1,18 @@
+require "./test/test_helper"
+
+class LeagueStatisticsTest < MiniTest::Test
+
+  def setup
+    game_path = './data/games.csv'
+    team_path = './data/teams.csv'
+    game_teams_path = './data/game_teams.csv'
+
+    locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
+
+    @league_statistics = LeagueStatistics.new(locations)
+  end
+end
