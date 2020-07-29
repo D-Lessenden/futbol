e86b1d10192d1a843b7efea11568eab142b926e4
Arique Aguilar <arique1104@gmail.com>
2020-07-29 14:38:19 -0500

    Test it has attributes passing for teamstatistics

--- test/csv_data_test.rb
+++ test/csv_data_test.rb
@@ -5,8 +5,7 @@ require "./lib/stat_tracker"
 require "./lib/csv_data"
 
 class CSVDataTest < Minitest::Test
-
-  def test_it_exists
+  def setup
     game_path = './data/games.csv'
     team_path = './data/teams.csv'
     game_teams_path = './data/game_teams.csv'
@@ -17,11 +16,19 @@ class CSVDataTest < Minitest::Test
       game_teams: game_teams_path
     }
     StatTracker.from_csv(locations)
-    csv_data = CSVData.new(locations)
-    assert_instance_of CSVData, csv_data
+    @csv_data = CSVData.new(locations)
+
   end
 
-  #def test_it_has_attributes
-  #end
+  def test_it_exists
+    assert_instance_of CSVData, @csv_data
+  end
+
+  def test_it_has_attributes
+    #assert_equal GamesStatistics, @csv_data.game_statistics.class
+    #assert_equal LeagueStatistics, csv_data.league_statistics.class
+    #assert_equal SeasonStatistics, csv_data.season_statistics
+    assert_equal TeamStatistics, @csv_data.team_statistics.class
+  end
 
 end
