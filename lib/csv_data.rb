d753dd5f780b61e71c6678523707bc0ae7e116cb
Garret Wolf <garretwolf26@gmail.com>
2020-07-29 16:09:03 -0400

    Add csv data class

--- lib/csv_data.rb
+++ lib/csv_data.rb
@@ -2,18 +2,18 @@ require "CSV"
 require "./lib/games"
 require "./lib/teams"
 require "./lib/game_teams"
-# require "./lib/team_statistics"
 require "./lib/league_statistics"
 
 class CSVData
  attr_reader  :league_statistics
+
  def initialize(locations)
    @games ||= turn_games_csv_data_into_games_objects(locations[:games])
    @teams ||= turn_teams_csv_data_into_teams_objects(locations[:teams])
    @game_teams ||= turn_game_teams_csv_data_into_game_teams_objects(locations[:game_teams])
-   # @team_statistics = TeamStatistics.new(@games, @teams, @game_teams)
    @league_statistics = LeagueStatistics.new(@games, @teams, @game_teams)
  end
+
  def turn_games_csv_data_into_games_objects(games_csv_data)
    games_objects_collection = []
    CSV.foreach(games_csv_data, headers: true, header_converters: :symbol, row_sep: :auto) do |row|
@@ -21,6 +21,7 @@ class CSVData
    end
    games_objects_collection
  end
+
  def turn_teams_csv_data_into_teams_objects(teams_csv_data)
    teams_objects_collection = []
    CSV.foreach(teams_csv_data, headers: true, header_converters: :symbol, row_sep: :auto) do |row|
@@ -28,6 +29,7 @@ class CSVData
    end
    teams_objects_collection
  end
+ 
  def turn_game_teams_csv_data_into_game_teams_objects(game_teams_csv_data)
    game_teams_objects_collection = []
    CSV.foreach(game_teams_csv_data, headers: true, header_converters: :symbol, row_sep: :auto) do |row|
